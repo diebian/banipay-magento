cb4793d11e66d6ce3076e833fbf1cd6631d987f0
Diego Mamani Zanca <dmz3010@gmail.com>
2020-09-30 22:48:29 -0400

    logger

---
+++ BaniPay/Logger/Handler.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace BaniPayPaymentGateway3\BaniPay\Logger;
+ 
+use Monolog\Logger;
+ 
+class Handler extends \Magento\Framework\Logger\Handler\Base
+{
+    protected $loggerType = Logger::INFO;
+ 
+    // protected $fileName = '/var/log/banipay.log';
+}
---
+++ BaniPay/Logger/Logger.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace BaniPayPaymentGateway3\BaniPay\Logger;
+ 
+class Logger extends \Monolog\Logger
+{
+}
