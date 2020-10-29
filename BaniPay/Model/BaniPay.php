533e81bdeb44c32acbd079077473cc33a523d2f6
Diego Mamani <dmz3010@gmail.com>
2020-10-29 10:29:16 +0000

    get affiliate code

--- BaniPay/Model/BaniPay.php
+++ BaniPay/Model/BaniPay.php
@@ -21,4 +21,116 @@ class BaniPay extends \Magento\Payment\Model\Method\AbstractMethod
 
     protected $_logger;
 
+
+    public $transaction = array();
+
+    private $const = array();
+
+    private $payload = array();
+
+    private $transactionGenerated = array();
+    private $transactionStatus = array();
+    private $affiliate = array();
+
+    private $urlTransaction = "https://banipay.me:8443/api/payments/transaction";
+    private $urlTransactionInfo = "https://banipay.me:8443/api/payments/info";
+    private $urlAffiliate = "https://banipay.me:8443/api/affiliates";
+
+
+    public function getTest(){
+        return 'message from model banipay';
+    }
+
+    public function register ($data, $params){
+        $this->transaction = $data;
+        $this->const = array(
+            "affiliateCode"   => $params['affiliateCode'],
+            "expireMinutes"   => $params['expireMinutes'],
+            "failedUrl"       => $params['failedUrl'],
+            "successUrl"      => $params['successUrl'],
+            // "notificationUrl" => $params['notificationUrl']
+        );
+        if( self::verify() ) {
+            $data = self::toComplete();
+            return self::send();
+        } else {
+            return "Incorrectly formatted data";
+        }
+    }
+
+    private function verify() {
+        if (is_array($this->transaction)) {
+            foreach ($this->keys as $key => $value) {
+                if ( !array_key_exists($value, $this->transaction) ) return false;
+            }
+            return true;
+        } else {
+            return false;
+        } 
+    }
+
+    private function toComplete(){
+        $this->payload = $this->const + $this->transaction;
+        return $this->payload;
+    }
+
+    public function getPayload(){
+        return json_encode($this->payload);
+    }
+
+    private function send(){
+        $ch = curl_init($this->urlTransaction);
+        $payload = self::getPayload();
+
+        // Attach encoded JSON string to the POST fields
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        // Set the content type to application/json
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+        // Return response instead of outputting
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Execute the POST request
+        $this->transactionGenerated = json_decode(curl_exec($ch));
+
+        // Close cURL resource
+        curl_close($ch);
+
+        return $this->transactionGenerated;
+    }
+
+    public function getTransaction ($paymentId, $transactionGenerated){
+
+        $ch = curl_init("{$this->urlTransactionInfo}/{$paymentId}/{$transactionGenerated}");
+
+        // Return response instead of outputting
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Execute the POST request
+        $this->transactionStatus = json_decode(curl_exec($ch));
+
+        // Close cURL resource
+        curl_close($ch);
+
+        return $this->transactionStatus;
+    }
+
+    public function getAffiliate ($affiliateCode) {
+
+        $ch = curl_init("{$this->urlAffiliate}/{$affiliateCode}");
+
+        // Return response instead of outputting
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Execute the POST request
+        $this->affiliate = json_decode(curl_exec($ch));
+
+        // Close cURL resource
+        curl_close($ch);
+
+        return $this->affiliate;
+
+    }
+
 }
--- BaniPay/Observer/Redirect.php
+++ BaniPay/Observer/Redirect.php
@@ -8,6 +8,9 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Message\ManagerInterface;
 use \Psr\Log\LoggerInterface;
 
+use BaniPayPaymentGateway3\BaniPay\Model\BaniPay;
+
+use Magento\Framework\Controller\ResultFactory;
 
 
 class Redirect implements ObserverInterface
@@ -17,13 +20,24 @@ class Redirect implements ObserverInterface
     protected $_responseFactory;
     protected $_url;
     protected $_logger;
-    
+
+    protected $banipay;
+
+    public $details = array();
+    public $transaction = array();
+
+    const REDIRECT_URL = 'https://diebian.dev';
+    protected $resultRedirect;   
 
     public function __construct(\Magento\Framework\Message\ManagerInterface $messageManager,
         \Magento\Framework\App\ResponseFactory $responseFactory,
         \Magento\Framework\UrlInterface $url,
         // \Magento\Payment\Model\Method\Logger $logger
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+
+        BaniPay $banipay,
+
+        \Magento\Framework\Controller\ResultFactory $result
         
     ) {
         $this->messageManager = $messageManager;
@@ -31,13 +45,13 @@ class Redirect implements ObserverInterface
         $this->_url = $url;
         $this->_logger = $logger;
 
+        $this->banipay = $banipay;
+
+        $this->resultRedirect = $result;
 
     }
 
-    public function execute(EventObserver $observer) {
-    // public function execute (\Magento\Framework\Event\Observer $observer) {
-        
-
+    public function execute(EventObserver $observer) {       
      
         $this->_logger->info('from OBSERVER root: info ORDER');
         // $this->_logger->debug(var_dump($order , true));
@@ -45,18 +59,83 @@ class Redirect implements ObserverInterface
         // $this->_logger->debug(print_r($order->getState(), true));
         $this->_logger->debug('from OBSERVER root: diebiandev diebiandev');
         
-        $this->_logger->debug(print_r($observer->getEvent()->debug(), true));
+        $order = $observer->getEvent()->debug();
+        // $order = $observer->getEvent()->getOrder();    
+        $this->_logger->debug(print_r($order , true));
+        
+        $order = $observer->getEvent()->getOrder();
+        $increment_id = $order->getincrement_id();
+        $items = $order->getAllVisibleItems();
+
+        // Products loading
+        foreach($items as $item){
+            $this->_logger->debug(print_r($item->getName(), true));
+            $this->_logger->debug(print_r($item->getPrice(), true));
+            $this->_logger->debug(print_r($item->getQtyOrdered(), true));
+
+            $data = array(
+                "concept"         => $item->getName(),
+                "productImageUrl" => $item->getThumbnail(),
+                "quantity"        => $item->getQtyOrdered(),
+                "unitPrice"       => $item->getPrice(),
+            );
+            array_push($this->details, $data);   
+        }
+
+        // Products services to register
+        $data = array(
+            "withInvoice"        => false,
+            "externalCode"       => '$_COOKIE["woocommerce_cart_hash"]',
+            "paymentDescription" =>  'get_bloginfo()',
+            
+            "address"            => '$order->get_billing_address_1()', 
+            "administrativeArea" => '$order->get_billing_state()', 
+            "country"            => '$order->get_billing_country()', 
+            "firstName"          => '$order->get_billing_first_name()', 
+            "identifierCode"     => '$order_id', 
+            "identifierName"     => '$order->get_billing_first_name()', 
+            "lastName"           => '$order->get_billing_last_name()', 
+            "locality"           => '$order->get_billing_city()', 
+            "email"              => '$order->get_billing_email()', 
+            "nit"                => '$this->checkBilling(  )', 
+            "nameOrSocialReason" => '$this->checkBilling( )',  
+            "phoneNumber"        => '$order->get_billing_phone()', 
+            "postalCode"         => '$order->get_billing_postcode()',
+            
+            "details"            => $this->details
+        );
+
+        // Settings needed to record transaction
+        $params = array(
+            "affiliateCode"   => '141581ae-fb1f-4cfb-b21e-040a8851c265',
+            "expireMinutes"   => 30,
+            "failedUrl"       => 'https://facebook.com',
+            "successUrl"      =>  'https://diebian.dev'
+        );
+
+        $affiliate = $this->banipay->getAffiliate('141581ae-fb1f-4cfb-b21e-040a8851c265');
+        $this->_logger->debug(print_r($affiliate , true));
+        
+        $this->_logger->debug(print_r($this->details , true));
 
         $result          = $observer->getEvent()->getResult();
         $method_instance = $observer->getEvent()->getMethodInstance();
         // $this->_logger->debug(print_r($observer->getEvent(), true));
 
-        // $event = $observer->getEvent();
-        // $this->redirect->redirect($event->getResponse(), 'https://diebian.dev');
 
-        // var_dump($order);
-        // return true;
+        $this->_logger->debug(print_r($this->banipay->getTest(), true));
 
+
+        /* $resultRedirect = $this->resultRedirect->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setUrl(self::REDIRECT_URL);
+        return $resultRedirect; */
+
+        $observer->getEvent()->getFront()->getResponse()->setRedirect('https://diebian.dev')->sendResponse();
+        $observer->getEvent()->getResponse()->setRedirect($url)->sendResponse();
+        exit;
+
+
+        return;
 
         exit;
     }
